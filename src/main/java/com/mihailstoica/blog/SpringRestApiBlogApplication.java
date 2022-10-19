47298ebbe219bd3e0e075d6780f6fe329e4faa6b
Mihail Stoica <90096221+mihail-stoica@users.noreply.github.com>
2022-10-19 12:23:11 +0200

    Initial commit

---
+++ src/main/java/com/mihailstoica/blog/SpringRestApiBlogApplication.java
@@ -0,0 +1,13 @@
+package com.mihailstoica.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestApiBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mihailstoica/blog/SpringRestApiBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mihailstoica.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
