6d8b246933cc0b89ccafb02b303f329950bbb6ed
Mihail Stoica <90096221+mihail-stoica@users.noreply.github.com>
2022-10-21 09:47:20 +0200

    Add unit tests for PostController endpoints methods createPost & getAllPosts

--- src/test/java/com/mihailstoica/blog/controller/PostControllerTests.java
+++ src/test/java/com/mihailstoica/blog/controller/PostControllerTests.java
@@ -1,11 +1,31 @@
 package com.mihailstoica.blog.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mihailstoica.blog.entity.Post;
+import com.mihailstoica.blog.payload.PostDto;
 import com.mihailstoica.blog.repository.PostRepository;
+import com.mihailstoica.blog.service.PostService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 public class PostControllerTests {
@@ -14,9 +34,67 @@ public class PostControllerTests {
     private MockMvc mockMvc;
 
     @MockBean
-    private PostRepository postRepository;
+    private PostService postService;
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    private Post post;
+
+    private PostDto postDto;
+
+    @BeforeEach
+    public void setup() {
+        this.post = new Post();
+        this.post.setTitle("Title");
+        this.post.setDescription("Description");
+        this.post.setContent("Content");
+
+        this.postDto = new PostDto();
+        this.postDto.setId(post.getId());
+        this.postDto.setTitle(post.getTitle());
+        this.postDto.setDescription(post.getDescription());
+        this.postDto.setContent(post.getContent());
+    }
+    @DisplayName("JUnit test for createPost")
+    @Test
+    public void givenPostDtoObject_whenCreatePost_thenReturnCreatedPostDto() throws Exception {
+
+        // given - precondition or setup
+        // stub method for postService.createPost
+        given(postService.createPost(any(PostDto.class)))
+                .willAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
+
+        // when - action or behaviour that we are going to test
+        ResultActions response = mockMvc.perform(post("/api/posts")
+                .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(postDto)));
+
+        // then - verify the output
+        response.andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id", is(postDto.getId())))
+                .andExpect(jsonPath("$.title", is(postDto.getTitle())))
+                .andExpect(jsonPath("$.description", is(postDto.getDescription())))
+                .andExpect(jsonPath("$.content", is(postDto.getContent())));
+    }
+
+    @DisplayName("JUnit test for getAllPosts")
+    @Test
+    public void givenListOfPostDto_whenGetAllPosts_thenReturnPostDtoList() throws Exception {
+
+        // given - precondition or setup
+        List<PostDto> listOfPostDto = List.of(postDto);
+        //stub method for postService.getAllPosts
+        given(postService.getAllPosts()).willReturn(listOfPostDto);
+
+        // when - action or behaviour that we are going to test
+        ResultActions response = mockMvc.perform(get("/api/posts"));
+
+        // then - verify the output
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.size()", is(listOfPostDto.size())));
+    }
 
 }
