269654ee3c3b2a128c2a29b3f21d5d9324356c08
Mihail Stoica <90096221+mihail-stoica@users.noreply.github.com>
2022-10-25 12:13:23 +0200

    Refactoring hardcoded values for paging and sorting

--- src/main/java/com/mihailstoica/blog/controller/PostController.java
+++ src/main/java/com/mihailstoica/blog/controller/PostController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static com.mihailstoica.blog.utils.Constants.*;
+
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
@@ -25,10 +27,10 @@ public class PostController {
 
     @GetMapping
     public ResponseEntity<PostResponse> getAllPosts(
-            @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
-            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
-            @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy,
-            @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir) {
+            @RequestParam(value = "pageNo", defaultValue = DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = DEFAULT_SORT_DIRECTION, required = false) String sortDir) {
 
         return new ResponseEntity<>(postService.getAllPosts(pageNo, pageSize, sortBy, sortDir), HttpStatus.OK);
     }
