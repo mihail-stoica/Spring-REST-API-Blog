6e3e502c4e9a889ab8f9ddf604165c372423ca99
Mihail Stoica <90096221+mihail-stoica@users.noreply.github.com>
2022-11-02 11:21:58 +0100

    Add CommentRepository and CommentDto

---
+++ src/main/java/com/mihailstoica/blog/payload/CommentDto.java
@@ -0,0 +1,16 @@
+package com.mihailstoica.blog.payload;
+
+import lombok.Data;
+
+@Data
+public class CommentDto {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String body;
+
+}
